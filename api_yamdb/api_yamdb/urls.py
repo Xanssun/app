83cb79418f65ff3ca8acaee04c89a88ccb914aee
Xanssun <115156090+Xanssun@users.noreply.github.com>
2023-05-29 16:55:26 +0600

    testnew

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from django.contrib import admin
-from django.urls import include, path 
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
